f101a2860f8b48693c95fd0f6c99969f23b05bad
krook1024 <krook1024@gmail.com>
2019-03-07 23:04:02 +0100

    XorEncode in java done

--- files/caesar/java/XorEncode.java
+++ files/caesar/java/XorEncode.java
@@ -1,6 +1,33 @@
-class XorEncode {
-   public static void main(String[] args){
-        System.out.println("java sucks horse****");
-   }
-}
+import java.util.*;
 
+class XorEncode {
+	public static void main(String[] args) {
+		String kulcs = "";
+
+		if(args.length > 0) {
+			kulcs = args[0];
+		} else {
+			System.out.println("Kulcs nelkul nem titkositok!");
+			System.out.println("Hasznalat: java XorEncode.java [kulcs]");
+			System.exit(-1);
+		}
+
+		Scanner sc = new Scanner(System.in);
+		String str = "";
+
+		while(sc.hasNext()) {
+			str = sc.next();
+			System.out.println(xor(kulcs, str));
+		}
+	}
+
+	public static String xor(String kulcs, String s) {
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 0; i < s.length(); i++) {
+			sb.append((char)(s.charAt(i) ^ kulcs.charAt(i % kulcs.length())));
+		}
+
+		return sb.toString();
+	}
+}
