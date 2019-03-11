4cb8b49a921273a594d7718a0516410a9f755705
krook1024 <krook1024@gmail.com>
2019-03-11 13:31:10 +0100

    programlisting -> screen where it's more appropiate

---
+++ files/chomsky/hiba_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int tomb[5] = {1, 2, 3, 4, 5};
+	for(int i=0; i < 5; tomb[i]= i++) {
+		printf("%d %d\n", i, tomb[i]);
+	}
+
+	return 0;
+}
---
+++ files/chomsky/hiba_5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 4,
+		b = 4,
+		n = 5,
+		*d = &a,
+		*s = &b;
+
+	for(int i=0; i < n && (*d++ = *s++); ++i)
+	{
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
