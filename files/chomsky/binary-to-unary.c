fadb63e30bff832249144c06cf51f9597b4c162e
krook1024 <krook1024@gmail.com>
2019-03-12 19:21:37 +0100

    chomsky

--- files/chomsky/binary-to-unary.c
+++ files/chomsky/binary-to-unary.c
@@ -5,12 +5,13 @@ int main(void)
 	printf
 	    ("Please enter a decimal number you'd like to convert to unary: ");
 
-	int in = 0;
+	unsigned int in = 0;
 	scanf("%d", &in);
 
 	for (int i = 0; i < in; ++i)
 		(i % 5) ? printf("|") : printf(" |");
 
 	printf("\n");
+
 	return 0;
 }
