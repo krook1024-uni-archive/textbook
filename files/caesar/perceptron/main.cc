1cf96ad684dc92e4c6f8d962a485d17878568e53
krook1024 <krook1024@gmail.com>
2019-03-20 20:39:39 +0100

    Perceptronos feladat (Caesar)

--- files/caesar/perceptron/main.cc
+++ files/caesar/perceptron/main.cc
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <png++/png.hpp>
+#include "ql.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
+	if(argc < 1) {
+		std::cout << "Too few arguments passed!" << std::endl;
+	}
+
+	png::image<png::rgb_pixel> image(argv[1]);
+
+	int size = image.get_width() * image.get_height();
+	
+	Perceptron *p = new Perceptron(3, size, 256, 1);
+
+	double *image_d = new double[size];
+
+	for(int i = 0; i < image.get_width(); i++)
+		for(int j = 0; j < image.get_height(); j++)
+			image_d[i*image.get_width()+j] = image[i][j].red;
+
+	double value = (*p)(image_d);
+
+	std::cout << value << std::endl;
+
+	delete p;
+	delete[] image_d;
 
 	return 0;
 }
