b5bada70ca47b42e1385d833a1b8e20dd630ae4c
krook1024 <krook1024@gmail.com>
2019-04-11 09:00:38 +0200

    java életjáték

---
+++ files/conway/java_elet/Eletjatek.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+class Eletjatek {
+
+	@Override
+	public void start(Stage stage) {
+		Scene scene = new Scene(/* TODO */, 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] arguments) {
+		// TODO
+		launch();
+	}
+}
