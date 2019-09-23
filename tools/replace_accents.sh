d0c00c883182381cd2caf46b29980d4dc12ed448
krook1024 <krook1024@gmail.com>
2019-09-23 19:42:21 +0200

    fix

--- tools/replace_accents.sh
+++ tools/replace_accents.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
 
-accents=('á' 'é' 'í' 'ó' 'ö' 'ő' 'ú' 'ü' 'ű')
-
-cd ../files/
-
-for i in "${accents[@]}"; do
-    echo $i
-    egrep -lRZ $i . | xargs -0 -l sed -i -e "s/$i//g"
+cd files
+for f in $(find . -name "*" ! -path '*venv*' ! -path '*png' ! -path *'jpg' -type f); do
+    echo $f
+    cat "$f" | iconv -f utf8 -t ascii//TRANSLIT//IGNORE > "$f.new"
+    mv "$f.new" "$f"
 done
-
