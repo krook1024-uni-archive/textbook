da061729b2cce8763a851fd63e323c4f7a808818
krook1024 <krook1024@gmail.com>
2019-10-14 08:36:20 +0200

    progress

--- files/conway/Sejtauto/sejtszal.cpp
+++ files/conway/Sejtauto/sejtszal.cpp
@@ -1,9 +1,9 @@
 // sejtszal.cpp
 //
-// …letj·tÈk rajzolÛ
-// ProgramozÛ P·ternoszter
+// √âletj√°t√©k rajzol√≥
+// Programoz√≥ P√°ternoszter
 //
-// Copyright (C) 2011, B·tfai Norbert, nbatfai@inf.unideb.hu, nbatfai@gmail.com
+// Copyright (C) 2011, B√°tfai Norbert, nbatfai@inf.unideb.hu, nbatfai@gmail.com
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,30 +18,30 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-// Ez a program szabad szoftver; terjeszthetı illetve mÛdosÌthatÛ a
-// Free Software Foundation ·ltal kiadott GNU General Public License
-// dokumentum·ban leÌrtak; ak·r a licenc 3-as, ak·r (tetszıleges) kÈsıbbi
-// v·ltozata szerint.
+// Ez a program szabad szoftver; terjeszthet√µ illetve m√≥dos√≠that√≥ a
+// Free Software Foundation √°ltal kiadott GNU General Public License
+// dokumentum√°ban le√≠rtak; ak√°r a licenc 3-as, ak√°r (tetsz√µleges) k√©s√µbbi
+// v√°ltozata szerint.
 //
-// Ez a program abban a remÈnyben ker¸l kˆzread·sra, hogy hasznos lesz,
-// de minden egyÈb GARANCIA N…LK‹L, az ELADHAT”S¡GRA vagy VALAMELY C…LRA
-// VAL” ALKALMAZHAT”S¡GRA valÛ sz·rmaztatott garanci·t is beleÈrtve.
-// Tov·bbi rÈszleteket a GNU General Public License tartalmaz.
+// Ez a program abban a rem√©nyben ker√ºl k√∂zread√°sra, hogy hasznos lesz,
+// de minden egy√©b GARANCIA N√âLK√úL, az ELADHAT√ìS√ÅGRA vagy VALAMELY C√âLRA
+// VAL√ì ALKALMAZHAT√ìS√ÅGRA val√≥ sz√°rmaztatott garanci√°t is bele√©rtve.
+// Tov√°bbi r√©szleteket a GNU General Public License tartalmaz.
 //
-// A felhaszn·lÛnak a programmal egy¸tt meg kell kapnia a GNU General
-// Public License egy pÈld·ny·t; ha mÈgsem kapta meg, akkor
+// A felhaszn√°l√≥nak a programmal egy√ºtt meg kell kapnia a GNU General
+// Public License egy p√©ld√°ny√°t; ha m√©gsem kapta meg, akkor
 // tekintse meg a <http://www.gnu.org/licenses/> oldalon.
 //
 //
 // Version history:
 //
-// 0.0.1    A kÈt oszt·ly tervezÈsÈnek fı szempontja az volt, hogy
-// ne vagy alig k¸lˆnbˆzzˆn az elsı C++-os pÈld·tÛl, a MandelostÛl:
+// 0.0.1    A k√©t oszt√°ly tervez√©s√©nek f√µ szempontja az volt, hogy
+// ne vagy alig k√ºl√∂nb√∂zz√∂n az els√µ C++-os p√©ld√°t√≥l, a Mandelost√≥l:
 // http://progpater.blog.hu/2011/02/26/tan_csodallak_amde_nem_ertelek_de_kepzetem_hegyvolgyedet_bejarja
-// ezÈrt az olyan kÈnyesebb dolgokkal, hogy kezelj¸k a racsIndex-et a
-// kÈt oszt·lyra bontott C++ megold·sban, amikor Ìrjuk ·t a Jav·sbÛl, nem foglalkoztunk
-// a kiindulÛ Jav·s: http://www.tankonyvtar.hu/informatika/javat-tanitok-1-2-080904-1
-// (a baz·r eszme: ÑRelease Early, Release Often" Ìrjuk ki a posztra)
+// ez√©rt az olyan k√©nyesebb dolgokkal, hogy kezelj√ºk a racsIndex-et a
+// k√©t oszt√°lyra bontott C++ megold√°sban, amikor √≠rjuk √°t a Jav√°sb√≥l, nem foglalkoztunk
+// a kiindul√≥ Jav√°s: http://www.tankonyvtar.hu/informatika/javat-tanitok-1-2-080904-1
+// (a baz√°r eszme: ¬ÑRelease Early, Release Often" √≠rjuk ki a posztra)
 //
 
 #include "sejtszal.h"
@@ -58,24 +58,24 @@ SejtSzal::SejtSzal(bool ***racsok, int szelesseg, int magassag, int varakozas, S
 }
 
 /**
-   * Az kÈrdezett ·llapotban lÈvı nyolcszomszÈdok sz·ma.
+   * Az k√©rdezett √°llapotban l√©v√µ nyolcszomsz√©dok sz√°ma.
    *
-   * @param   r·cs    a sejttÈr r·cs
-   * @param   sor     a r·cs vizsg·lt sora
-   * @param   oszlop  a r·cs vizsg·lt oszlopa
-   * @param   ·llapor a nyolcszomszÈdok vizsg·lt ·llapota
-   * @return int a kÈrdezett ·llapotbeli nyolcszomszÈdok sz·ma.
+   * @param   r√°cs    a sejtt√©r r√°cs
+   * @param   sor     a r√°cs vizsg√°lt sora
+   * @param   oszlop  a r√°cs vizsg√°lt oszlopa
+   * @param   √°llapor a nyolcszomsz√©dok vizsg√°lt √°llapota
+   * @return int a k√©rdezett √°llapotbeli nyolcszomsz√©dok sz√°ma.
    */
 int SejtSzal::szomszedokSzama(bool **racs,
                               int sor, int oszlop, bool allapot) {
     int allapotuSzomszed = 0;
-    // A nyolcszomszÈdok vÈgigzongor·z·sa:
+    // A nyolcszomsz√©dok v√©gigzongor√°z√°sa:
     for(int i=-1; i<2; ++i)
         for(int j=-1; j<2; ++j)
-            // A vizsg·lt sejtet mag·t kihagyva:
+            // A vizsg√°lt sejtet mag√°t kihagyva:
             if(!((i==0) && (j==0))) {
-        // A sejttÈrbıl szÈlÈnek szomszÈdai
-        // a szembe oldalakon ("periÛdikus hat·rfeltÈtel")
+        // A sejtt√©rb√µl sz√©l√©nek szomsz√©dai
+        // a szembe oldalakon ("peri√≥dikus hat√°rfelt√©tel")
         int o = oszlop + j;
         if(o < 0)
             o = szelesseg-1;
@@ -96,11 +96,11 @@ int SejtSzal::szomszedokSzama(bool **racs,
 }
 
 /**
- * A sejttÈr idıbeli fejlıdÈse a John H. Conway fÈle
- * Èletj·tÈk sejtautomata szab·lyai alapj·n tˆrtÈnik.
- * A szab·lyok rÈszletes ismertetÈsÈt l·sd pÈld·ul a
- * [MATEK J¡T…K] hivatkoz·sban (Cs·k·ny BÈla: DiszkrÈt
- * matematikai j·tÈkok. Polygon, Szeged 1998. 171. oldal.)
+ * A sejtt√©r id√µbeli fejl√µd√©se a John H. Conway f√©le
+ * √©letj√°t√©k sejtautomata szab√°lyai alapj√°n t√∂rt√©nik.
+ * A szab√°lyok r√©szletes ismertet√©s√©t l√°sd p√©ld√°ul a
+ * [MATEK J√ÅT√âK] hivatkoz√°sban (Cs√°k√°ny B√©la: Diszkr√©t
+ * matematikai j√°t√©kok. Polygon, Szeged 1998. 171. oldal.)
  */
 void SejtSzal::idoFejlodes() {
 
@@ -113,15 +113,15 @@ void SejtSzal::idoFejlodes() {
             int elok = szomszedokSzama(racsElotte, i, j, SejtAblak::ELO);
 
             if(racsElotte[i][j] == SejtAblak::ELO) {
-                /* …lı Èlı marad, ha kettı vagy h·rom Èlı
-             szomszedja van, k¸lˆnben halott lesz. */
+                /* √âl√µ √©l√µ marad, ha kett√µ vagy h√°rom √©l√µ
+             szomszedja van, k√ºl√∂nben halott lesz. */
                 if(elok==2 || elok==3)
                     racsUtana[i][j] = SejtAblak::ELO;
                 else
                     racsUtana[i][j] = SejtAblak::HALOTT;
             }  else {
-                /* Halott halott marad, ha h·rom Èlı
-             szomszedja van, k¸lˆnben Èlı lesz. */
+                /* Halott halott marad, ha h√°rom √©l√µ
+             szomszedja van, k√ºl√∂nben √©l√µ lesz. */
                 if(elok==3)
                     racsUtana[i][j] = SejtAblak::ELO;
                 else
@@ -133,7 +133,7 @@ void SejtSzal::idoFejlodes() {
 }
 
 
-/** A sejttÈr idıbeli fejlıdÈse. */
+/** A sejtt√©r id√µbeli fejl√µd√©se. */
 void SejtSzal::run()
 {
     while(true) {
