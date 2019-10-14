85a7af1d2d2dbdc8e0d354da4e82152e15ef94a5
krook1024 <krook1024@gmail.com>
2019-10-14 08:44:20 +0200

    progress

--- tools/replace_accents.sh
+++ tools/replace_accents.sh
@@ -3,6 +3,6 @@
 cd files
 for f in $(find . -name "*" ! -path '*venv*' ! -path '*png' ! -path *'jpg' ! -path *'.tar.gz' -type f); do
     echo $f
-    cat "$f" | iconv -c -f utf-8 -t ascii//TRANSLIT//IGNORE > "$f.new"
+    cat "$f" | iconv -f utf-8 -t ascii//TRANSLIT//IGNORE > "$f.new"
     mv "$f.new" "$f"
 done
