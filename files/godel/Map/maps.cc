33c320ccf150a000402189bdb09b8081fdf0cdc0
krook1024 <krook1024@gmail.com>
2019-11-02 22:58:29 +0100

    progress

--- files/godel/Map/maps.cc
+++ files/godel/Map/maps.cc
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <map>
 
-#define map_int_itr std::map<int, int>::iterator
-
 int main(int argc, char *argv[]) {
     std::map<int, int> pairs;
     pairs.insert(std::pair<int, int> (1, 10));
@@ -11,7 +9,7 @@ int main(int argc, char *argv[]) {
     pairs.insert(std::pair<int, int> (4, 40));
     pairs.insert(std::pair<int, int> (5, 50));
 
-    for(map_int_itr itr = pairs.begin(); itr != pairs.end(); ++itr) {
+    for(auto itr = pairs.begin(); itr != pairs.end(); ++itr) {
         std::cout << itr->first << "\t" << itr->second << std::endl;
     }
 
